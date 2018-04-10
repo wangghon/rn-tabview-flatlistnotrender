eeea0133235aeb525f94bd381789b2d738ea40ae
Hongbo wang <hongbo.wang@salmat.com.au>
2018-04-10 10:58:08 +1000

    add config data of contacts and groups

---
+++ FlatlistNotRender/app/config/contacts.js
@@ -0,0 +1,20 @@
+const contacts =
+  [
+    {
+      key: '1',
+      name: 'Tom',
+      mobile: '0431123321',
+    },
+    {
+      key: '2',
+      name: 'John',
+      mobile: '0431456654',
+    },
+    {
+      key: '3',
+      name: 'Fiona',
+      mobile: '0431654321',
+    },
+  ];
+
+export default contacts;
---
+++ FlatlistNotRender/app/config/groups.js
@@ -0,0 +1,20 @@
+const contacts =
+  [
+    {
+      key: '1',
+      name: 'Group 1',
+      number: 12,
+    },
+    {
+      key: '2',
+      name: 'Group A',
+      mobile: 10,
+    },
+    {
+      key: '3',
+      name: 'Fiona',
+      mobile: '0431654321',
+    },
+  ];
+
+export default contacts;
