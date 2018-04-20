0ab3d1efdda9c04a58219acfd4d46ac18a79267c
Hongbo wang <hongbo.wang@salmat.com.au>
2018-04-20 13:30:21 +1000

    add dynamic tab

--- FlatlistNotRender/app/reducers/routes.js
+++ FlatlistNotRender/app/reducers/routes.js
@@ -1,11 +1,14 @@
 import { Reducer } from 'react-native-router-flux';
 
-export const customInitialState = {
-  scene: {},
-  refreshedScene: null,
-  scenesRefreshedOn: {},
-  isNavigating: false,
-};
+import {
+  KEY_HOME_WITH_TAB,
+  KEY_TABVIEW_SCREEN,
+  KEY_DELIVERIES_SCREEN,
+  KEY_SETTINGS_SCREEN,
+} from '../scenes';
+
+const screenA = [KEY_DELIVERIES_SCREEN, KEY_TABVIEW_SCREEN, KEY_SETTINGS_SCREEN];
+const screenB = [KEY_TABVIEW_SCREEN, KEY_SETTINGS_SCREEN];
 
 let initialRouting;
 // needs to fake params to bypass sanity checks
@@ -21,6 +24,19 @@ export default function(state, action) {
     case 'RootContainerInitialAction': {
       initialRouting = {...action.initialState};
       return action.initialState;
+    }
+    case 'LOGIN_SUCCESS': {
+      const children = action.payload.id === 'A' ? screenA : screenB;
+      return {
+        ...state,
+        scenes: {
+          ...state.scenes,
+          [KEY_HOME_WITH_TAB]: {
+            ...state.scenes[KEY_HOME_WITH_TAB],
+            children,
+          },
+        },
+      };
     }
     default:
       return routerReducer(state, action);
